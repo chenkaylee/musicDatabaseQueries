5b08bd9e40a0da633e01b591acc6e87e6770f493
Kaylee Chen <chen.kayl@northeastern.edu>
2024-02-14 16:19:26 -0800

    Adding query1

---
+++ query1_createMusicTable.sql
@@ -0,0 +1,8 @@
+-- Query 1: Create a new Table Music Video, that is a class of type Track (generalization) but adds the attribute Video director. A music video is a track. There cannot be a video without a track, and each track can have either no video or just one. 
+-- Auhtor: Kaylee Chen
+
+CREATE TABLE MusicVideo (
+    track_id INTEGER PRIMARY KEY,
+    videoDirector TEXT,
+    FOREIGN KEY (track_id) REFERENCES Tracks(track_id) ON DELETE CASCADE
+)
