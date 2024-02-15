6055be51bd1f3c581d6867b66b4b7860e5bb1553
Kaylee Chen <chen.kayl@northeastern.edu>
2024-02-14 16:31:42 -0800

    Adding query2

---
+++ query2_insertVideos.sql
@@ -0,0 +1,13 @@
+-- Query 2: Write the queries that insert at least 10 videos, respecting the problem rules.
+-- Author Kaylee Chen
+
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(1, "Director 1");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(2, "Director 2");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(3, "Director 3");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(4, "Director 4");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(5, "Director 5");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(6, "Director 6");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(7, "Director 7");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(8, "Director 8");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(9, "Director 9");
+INSERT INTO MusicVideo(track_id, videoDirector) VALUES(10, "Director 10");
